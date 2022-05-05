2617c04d101ef5915cf35ceafaeaabb7f525d0e4
Marko <marko.stojanovic1994@gmail.com>
2022-05-05 22:03:07 +0200

    feat: prisma setup and auth with next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import '../styles/global.css';
 
 import { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+};
 
 export default MyApp;
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,33 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { NextApiHandler } from 'next';
+import NextAuth from 'next-auth';
+import EmailProvider from 'next-auth/providers/email';
+import GitHubProvider from 'next-auth/providers/github';
+
+import { prisma } from '../../../lib/prisma';
+
+const options = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    EmailProvider({
+      server: {
+        host: process.env.SMTP_HOST,
+        port: Number(process.env.SMTP_PORT),
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASSWORD,
+        },
+      },
+      from: process.env.SMTP_FROM,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.SECRET,
+};
+
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+
+export default authHandler;
