55c0c49aa35ab1cb376282e98dfa7b6973999642
Marko <marko.stojanovic1994@gmail.com>
2022-05-16 22:14:55 +0200

    feat: login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,14 +3,67 @@ import type { FC } from 'react';
 
 const Login: FC = () => {
 	return (
-		<div className="flex h-screen items-center justify-center">
-			<div className="flex w-1/2 flex-row">
-				<div className="hidden w-1/2 md:block">
-					<Image src="/static/images/login-image.jpg" alt="Login food image" width="640" height="959" />
-				</div>
-				<div className="w-1/2">
-					<div className="p-8">
-						<h1>Login in to your account</h1>
+		<div className="flex h-screen items-center justify-center bg-[#E5F4EC]">
+			<div className="flex h-max flex-row md:w-[900px]">
+				<Image
+					src="/static/images/login-image.jpg"
+					alt="Login food image"
+					width="640"
+					height="959"
+					className="hidden md:block"
+				/>
+				<div className="bg-[#FAFAFA] md:w-[800px]">
+					<div className="flex h-full flex-col items-start justify-start p-14">
+						<h1 className="text-2xl font-bold">Login in to your account</h1>
+						<div className="p-6" />
+						<label htmlFor="email">Email</label>
+						<input
+							type="text"
+							name="email"
+							className="w-full rounded border-2 p-3 focus:border-gray-500 focus:outline-none focus:ring-gray-500"
+							placeholder="Enter your email"
+						/>
+						<div className="p-4" />
+						<label htmlFor="password">Password</label>
+						<input
+							type="text"
+							name="password"
+							className="w-full rounded border-2 p-3 focus:border-gray-500 focus:outline-none focus:ring-gray-500"
+							placeholder="Enter your password"
+						/>
+						<div className="p-5" />
+						<button
+							type="button"
+							className="w-full rounded bg-red-400 p-3 text-white transition-colors duration-300 hover:bg-red-500"
+						>
+							Login
+						</button>
+						<div className="relative my-5 flex w-full items-center py-5">
+							<div className="grow border-t border-gray-300"></div>
+							<span className="mx-4 shrink text-gray-400">OR</span>
+							<div className="grow border-t border-gray-300"></div>
+						</div>
+						<button
+							type="button"
+							className="inline-flex w-full items-center justify-center rounded bg-[#4285F4] p-3 text-white transition-colors duration-300 hover:bg-blue-600 focus:outline-none"
+						>
+							<svg
+								className="mr-2 -ml-1 h-4 w-4"
+								aria-hidden="true"
+								focusable="false"
+								data-prefix="fab"
+								data-icon="google"
+								role="img"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 488 512"
+							>
+								<path
+									fill="currentColor"
+									d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+								></path>
+							</svg>
+							Sign in with Google
+						</button>
 					</div>
 				</div>
 			</div>
