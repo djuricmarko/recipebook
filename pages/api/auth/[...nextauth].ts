2255df3a380d5c6929b6291cf796bcbb7a42037c
Marko <marko.stojanovic1994@gmail.com>
2022-05-20 16:19:03 +0200

    chore: update prisma to work planetscale

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,32 +6,13 @@ import GoogleProvider from 'next-auth/providers/google';
 import prisma from '../../../lib/prisma';
 
 const options = {
+	adapter: PrismaAdapter(prisma),
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-			checks: 'state',
-			authorization: {
-				params: {
-					prompt: 'consent',
-					access_type: 'offline',
-					response_type: 'code',
-				},
-			},
 		}),
-		// EmailProvider({
-		// 	server: {
-		// 		host: process.env.SMTP_HOST,
-		// 		port: Number(process.env.SMTP_PORT),
-		// 		auth: {
-		// 			user: process.env.SMTP_USER,
-		// 			pass: process.env.SMTP_PASSWORD,
-		// 		},
-		// 	},
-		// 	from: process.env.SMTP_FROM,
-		// }),
 	],
-	adapter: PrismaAdapter(prisma),
 	secret: process.env.SECRET,
 };
 
