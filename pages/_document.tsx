34b5bb794efbb1a98e01e430656a8b32768d728b
Marko <marko.stojanovic1994@gmail.com>
2022-05-27 20:40:03 +0200

    chore: favicons and meta for SEO

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,15 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
+				<link href="/static/favicons/favicon.ico" rel="shortcut icon" />
+				<link href="/static/favicons/site.webmanifest" rel="manifest" />
+				<link href="/static/favicons/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
+				<link href="/static/favicons/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
+				<link href="/static/favicons/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
+				<meta content="#ffffff" name="theme-color" />
+				<meta content="#ffffff" name="msapplication-TileColor" />
+				<meta content="14d2e73487fa6c71" name="yandex-verification" />
+				<meta content="eZSdmzAXlLkKhNJzfgwDqWORghxnJ8qR9_CHdAh5-xw" name="google-site-verification" />
 				<meta content="max-snippet:-1, max-image-preview:large, max-video-preview:-1" name="robots" />
 			</Head>
 			<body className="bg-white antialiased">
